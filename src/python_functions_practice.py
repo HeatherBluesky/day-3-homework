36d4a128f30ec270994463944a37cea0dbf5cf95
Heather <heather@Kierans-MacBook.local>
2023-03-22 15:42:10 +0000

    first test done

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -1,2 +1,50 @@
 def return_10():
     return 10
+
+def add(number_1, number_2):
+    return number_1 + number_2
+
+def subtract(number_1, number_2):
+    return number_1 - number_2
+
+def multiply(number_1, number_2):
+    return number_1 * number_2
+
+def divide(number_1, number_2):
+    return number_1 / number_2
+
+def length_of_string(string):
+    return len(string)
+
+def join_string(string_1, string_2):
+    return string_1 + string_2
+
+def add_string_as_number(num1, num2):
+    return int(num1) + int(num2)
+
+months = [
+    "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "Decmeber"
+]
+    
+def number_to_full_month_name(number):
+    return months[number-1]
+
+def number_to_full_month_name(number):
+    return months[number-1]
+
+def number_to_full_month_name(number):
+    return months[number-1]
+
+short_months = [
+    "Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"
+]
+
+def number_to_short_month_name(number):
+    return short_months[number-1]
+
+def number_to_short_month_name(number):
+    return short_months[number-1]
+
+def number_to_short_month_name(number):
+    return short_months[number-1]
+
